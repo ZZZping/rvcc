cafe79ea0b594b835ea8f2e03be4ef1dae52ad9d
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-10-12 21:33:58 +0800

    [194] 识别宽字符字面量

--- tokenize.c
+++ tokenize.c
@@ -309,8 +309,8 @@ static Token *readStringLiteral(char *Start) {
 }
 
 // 读取字符字面量
-static Token *readCharLiteral(char *Start) {
-  char *P = Start + 1;
+static Token *readCharLiteral(char *Start, char *Quote) {
+  char *P = Quote + 1;
   // 解析字符为 \0 的情况
   if (*P == '\0')
     errorAt(Start, "unclosed char literal");
@@ -543,9 +543,16 @@ Token *tokenize(File *File) {
 
     // 解析字符字面量
     if (*P == '\'') {
-      Cur->Next = readCharLiteral(P);
+      Cur->Next = readCharLiteral(P, P);
       Cur = Cur->Next;
       P += Cur->Len;
+      continue;
+    }
+
+    // Wide character literal
+    if (startsWith(P, "L'")) {
+      Cur = Cur->Next = readCharLiteral(P, P + 1);
+      P = Cur->Loc + Cur->Len;
       continue;
     }
 
